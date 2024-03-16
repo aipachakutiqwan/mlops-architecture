61fc61193014d1c5b4143016038835599d51e15d
c325018 <florenciopaucar@gmail.com>
2024-03-16 01:47:44 +0100

    added dataset and model training notebook

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ def get_app(arg_app_config_file: str, arg_log_config_file: str) -> FastAPI:
     config = get_app_config_parameters(arg_app_config_file)
     log_config = get_app_config_parameters(arg_log_config_file)
     fast_app = FastAPI(title=config['app_name'], version=config['app_version'],
-                       debug=config['is_debug'])
+                       debug=config['is_debug'], swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
     fast_app.include_router(API_ROUTER, prefix=config['api_prefix'])
     fast_app.add_event_handler('startup', start_app_handler(fast_app, config))
     configure_logging(config, log_config)
